1fe8fa913dcc2eaae065cb10e78c99954312ff2e
Prop4et <francostanco97@gmail.com>
2023-03-16 17:06:31 +0100

    started working on gateway

---
+++ executables/gateway/lora-config.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021 Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * 
+ */
+
+// LoRaWAN region to use, full list of regions can be found at: 
+//   http://stackforce.github.io/LoRaMac-doc/LoRaMac-doc-v4.5.1/group___l_o_r_a_m_a_c.html#ga3b9d54f0355b51e85df8b33fd1757eec
+#define LORAWAN_REGION                  LORAMAC_REGION_EU868
+
+#define DEV_EUI                        "e661640843133e22"
+
+// for the region
+#define LORAWAN_CHANNEL_MASK            NULL
