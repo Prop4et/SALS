36223d0bc46f3c76f971dc4768b7e4b7ebccbc6d
Prop4et <francostanco97@gmail.com>
2023-04-04 11:07:11 +0200

    added pulink instead of hello world

--- executables/hello-abp/hello_abp.c
+++ executables/hello-abp/hello_abp.c
@@ -31,8 +31,25 @@ int receive_length = 0;
 uint8_t receive_buffer[242];
 uint8_t receive_port = 0;
 
+struct uplink {//modified from 16 bits to 32 cause of expected 18 bytes
+    uint32_t id; 
+    int32_t temp; //-273.15 - 90.00 -> -27315 - 9000
+    uint32_t hum; //0.00 - 100.00 -> 0 - 10000
+    uint32_t press; //840.0 - 1013.25 [hPa] -> 8400 - 10133
+    uint32_t AQI; //50.0 - 500.0 -> 500 - 5000
+    uint32_t CO2; //600 - 10000 [ppm] -> 600 - 10000
+};
+
 int main( void )
 {
+    struct uplink pkt = {
+        .id = DEV_ID,
+        .temp = 0,
+        .hum = 0,
+        .press = 0,
+        .AQI = 0,
+        .CO2 = 0,
+    }; 
     // initialize stdio and wait for USB CDC connect
     stdio_init_all();
     sleep_ms(5000);
@@ -40,8 +57,8 @@ int main( void )
     printf("Pico LoRaWAN - Hello ABP\n\n");
 
     // uncomment next line to enable debug
-    // lorawan_debug(true);
-
+    lorawan_debug(true);
+    lorawan_erase_nvm();
     // initialize the LoRaWAN stack
     printf("Initilizating LoRaWAN ... ");
     if (lorawan_init_abp(&sx12xx_settings, LORAWAN_REGION, &abp_settings) < 0) {
@@ -73,12 +90,9 @@ int main( void )
         // since the last message was sent
         uint32_t now = to_ms_since_boot(get_absolute_time());
         
-        if ((now - last_message_time) > 5000) {
-            const char* message = "hello world!";
-
+        if ((now - last_message_time) > 30000) {
             // try to send an unconfirmed uplink message
-            printf("sending unconfirmed message '%s' ... ", message);
-            if (lorawan_send_unconfirmed(message, strlen(message), 2) < 0) {
+            if (lorawan_send_unconfirmed(&pkt, sizeof(struct uplink), 2) < 0) {
                 printf("failed!!!\n");
             } else {
                 printf("success!\n");
